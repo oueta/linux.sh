2f8e3bdcccaa558348fea2723ab1d61220380e09
Oueta <contact@oueta.com>
2017-12-11 21:14:14 +0200

    Init

---
+++ pinw.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Winstyle++ ping
+up=0;
+down=0;
+total=1;
+while(true)
+do
+ping=`ping -c 1 -W 1 $1 | grep from | cut -d: -f2`
+    if [ ! -z "$ping" ]; then
+        up=$((up+1));
+        echo -e "[`date +%H:%M:%S`] (total=$total; sent=$up; lost=$down)\e[32m Reply from $1:$ping \e[0m";
+		sleep 1;
+    else
+        down=$((down+1));
+        echo -e "[`date +%H:%M:%S`] (total=$total; sent=$up; lost=$down)\e[91m Request timed out.\e[0m";
+    fi
+total=$((total+1));
+done
