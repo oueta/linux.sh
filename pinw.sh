c12baed7c4e73340412da249811776c21e426cbc
Oueta <contact@oueta.com>
2017-12-13 20:23:12 +0200

    Added error handling

--- pinw.sh
+++ pinw.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
-# Winstyle++ ping
+# pinw.sh
+# winstyle++ ping
 if [ -z "$1" ]
-  then
-    echo "Usage: ./pinw.sh ip";
-    exit 1;
+then
+    echo "Usage: $0 ip"
+    exit 1
 fi
-up=0;
-down=0;
-total=1;
+up=0
+down=0
+total=0
 while(true)
 do
-ping=`ping -c 1 -W 1 $1 | grep from | cut -d: -f2`
-    if [ ! -z "$ping" ]; then
-        up=$((up+1));
-        echo -e "[`date +%H:%M:%S`] (total=$total; sent=$up; lost=$down)\e[32m Reply from $1:$ping \e[0m";
-		sleep 1;
+    ping=`ping -c 1 -W 1 $1`
+    ping_exit=$?
+    total=$((total+1))
+    ping=`echo "$ping" | grep ttl`
+    if [ ! -z "$ping" ]
+    then
+        up=$((up+1))
+        echo -e "[`date +%H:%M:%S`] (total=$total; sent=$up; lost=$down)\e[32m $ping \e[0m"
+		sleep 1
     else
-        down=$((down+1));
-        echo -e "[`date +%H:%M:%S`] (total=$total; sent=$up; lost=$down)\e[91m Request timed out.\e[0m";
+        if [ "$ping_exit" -eq "1" ]
+        then
+            down=$((down+1))
+            echo -e "[`date +%H:%M:%S`] (total=$total; sent=$up; lost=$down)\e[91m Request timed out.\e[0m"
+        else
+            break
+            exit $ping_exit
+		fi
     fi
-total=$((total+1));
 done
